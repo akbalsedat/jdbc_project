ac93cba0f717ec8001fe8b77ef3bc046705600a3
akbalsedat <akbalsedat@gmail.com>
2020-05-14 23:43:08 -0400

    some changes in InserAndDelete File

--- src/test/java/jdbctests/day1/InsertAndDeleteTest.java
+++ src/test/java/jdbctests/day1/InsertAndDeleteTest.java
@@ -3,17 +3,19 @@ package jdbctests.day1;
 import java.sql.*;
 
 public class InsertAndDeleteTest {
-    public static void main(String[] args) throws SQLException {
-        String oracleDbUrl = "jdbc:oracle:thin:@ec2-18-215-144-143.compute-1.amazonaws.com:1521:xe";
-        String oracleDbUsername = "hr";
-        String oracleDbPassword = "hr";
 
+    static String oracleDbUrl = "jdbc:oracle:thin:@ec2-18-215-144-143.compute-1.amazonaws.com:1521:xe";
+    static String oracleDbUsername = "hr";
+    static String oracleDbPassword = "hr";
+
+    public static void main(String[] args) throws SQLException {
         Connection connection = DriverManager.getConnection(oracleDbUrl, oracleDbUsername, oracleDbPassword);
         Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
                 ResultSet.CONCUR_READ_ONLY);
 
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM employees");
-        statement.executeQuery("INSERT INTO cybertek15 VALUES (9876,'Lynne', 'Davis', SYSDATE, 'F');");
+        ResultSet resultSet = statement.executeQuery(
+                "UPDATE cybertek15 SET START_DATE = '23-NOV-55' WHERE STUDENT_ID = 9988");
+
 
         // close connections
         resultSet.close();
