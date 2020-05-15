0b847907da62b5ca0e6ca3502a3726f9e1ccb34d
akbalsedat <akbalsedat@gmail.com>
2020-05-14 23:04:23 -0400

    examples with resultSetMetaData

--- src/test/java/jdbctests/Main.java
+++ src/test/java/jdbctests/Main.java
@@ -12,7 +12,7 @@ public class  Main {
         Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
                 ResultSet.CONCUR_READ_ONLY);
 
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM COUNTRIES");
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM employees");
         while (resultSet.next()) {
             System.out.println(resultSet.getString( 1) + " " +resultSet.getString(2) + " "
                     + resultSet.getString(3));
@@ -26,10 +26,27 @@ public class  Main {
         System.out.println("Database name: "+databaseMetaData.getDatabaseProductName());
         System.out.println("Database version: "+databaseMetaData.getDatabaseProductVersion());
 
-            // close connections
+        System.out.println("*****************************");
+        System.out.println("Number of column: "+resultSetMetaData.getColumnCount());
+        System.out.println("Label of 1st column: "+resultSetMetaData.getColumnName(1));
+
+        resultSet.beforeFirst();
+
+        for (int columnIndex = 1; columnIndex <= resultSetMetaData.getColumnCount(); columnIndex++) {
+            System.out.printf("%-15s", resultSetMetaData.getColumnName(columnIndex));
+        }
+
+        while(resultSet.next()) {
+            for (int columnIndex = 1; columnIndex <= resultSetMetaData.getColumnCount(); columnIndex++) {
+                System.out.printf("%-15s", resultSet.getString(columnIndex));
+            }
+            System.out.println(" ");
+        }
+
+
+        // close connections
         resultSet.close();
         statement.close();
         connection.close();
-
     }
 }
